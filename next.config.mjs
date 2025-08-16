c80a9e1a946e78fe0ce70a707c016a804739812f
yahya3579 <ninjab330@gmail.com>
2025-08-16 12:34:35 +0500

    yoo

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  },
-  // Disable server-side features for static export
-  experimental: {
-    appDir: true
-  }
+   
 };
 
 export default nextConfig;
